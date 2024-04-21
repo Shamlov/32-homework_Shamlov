2f7be72cf819757b891e1e900beeff327a50b4d4
Shamlov <pshamlov@gmail.com>
2024-04-21 18:46:13 +0300

    result V1.1

--- js/script.js
+++ js/script.js
@@ -39,20 +39,17 @@ function travelTime(automobile, time) {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-/*Создать объект, хранящий в себе отдельно числитель и знаменатель дроби, и следующие функции для работы с этим объектом.
-1. 
-2. Функция вычитания 2-х объектов-дробей.
-3. Функция умножения 2-х объектов-дробей.
-4. Функция деления 2-х объектов-дробей. */
+// Создать объект, хранящий в себе отдельно числитель и знаменатель дроби, и следующие функции для работы с этим объектом.
+
 
 
 let number1 = {
-    numerator: 5,
-    denominator: 10,
+    numerator: 15,
+    denominator: 23,
 }
 
 let number2 = {
-    numerator: 8,
+    numerator: 3,
     denominator: 12,
 }
 // Функция сложения 2-х объектов-дробей.
@@ -79,7 +76,7 @@ function sum(objA, objB) {
         numerator: numeratorA + numeratorB,
         denominator: rez
     }
-    return reducingFraction(rezObj)   // т.к. функции объевленные с помощью function всплываюют. мы можем вызвать ее
+    return reducingFraction(rezObj)   // т.к. функции объевленные с помощью function всплываюют. мы можем вызвать ее до объявления
 }
 
 // let sum2 = sum(number1, number2)
@@ -103,3 +100,124 @@ function reducingFraction(obj)  {    // функция сокращения др
 
 }
 // reducingFraction(sum2) 
+
+////////////////////////////////////////////////////////////////////////////////////
+// Функция вычитания 2-х объектов-дробей.
+
+function difference(objA, objB) {
+    let count = objA.denominator 
+    if(objA.denominator > objB.denominator ) {
+        count = objB.denominator
+    }              // нашли минимальный знаменатель
+    let rez = count
+    while(!(rez % objA.denominator  == rez % objB.denominator )) {
+        rez += count                  // каждый раз прибавляем наименьший знаменатель . что значительно сократит перебор
+    }
+    console.log('наименьший общий делитель ' + rez)     // наименьший общий делитель
+    // приведем дроби к общему знаменателю  (добавим множители к числителю)
+    let numeratorA = objA.numerator * (rez / objA.denominator)
+    let numeratorB = objB.numerator * (rez / objB.denominator)
+    console.log('вычесленный числитель числа A ' + numeratorA)
+    console.log('вычесленный числитель числа B ' + numeratorB)
+    console.log(`${numeratorA - numeratorB} / ${rez}`)  // только теперь дродь нужно сократить .
+    let rezObj =  {
+        numerator: numeratorA - numeratorB,
+        denominator: rez
+    }
+    return reducingFraction(rezObj)   
+
+}
+// difference(number1, number2) 
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Функция умножения 2-х объектов-дробей.
+function multiplication(objA, objB) {
+
+    let rezObj =  {
+        numerator: objA.numerator * objB.numerator,
+        denominator: objA.denominator * objB.denominator
+    }
+    return reducingFraction(rezObj)   
+}
+// console.log(multiplication(number1, number2))
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Функция деления 2-х объектов-дробей.
+
+function divide(objA, objB)  {
+    let rezObj =  {
+        numerator: objA.numerator * objB.denominator,
+        denominator: objA.denominator * objB.numerator
+    }
+    return reducingFraction(rezObj)  
+}
+// console.log(divide(number1, number2))     // числитель больше знаменателя. но не было задачи выбрать целые части числа.
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Создать объект, описывающий время (часы, минуты, секунды), и следующие функции для работы с этим объектом.
+
+const currentTime = {
+    hours: 1,
+    min: 40,
+    sec: 20,
+}
+
+// 1. Функция вывода времени на экран.
+function showTime(obj) {
+    console.log(`наше время ${obj.hours}:${obj.min}:${obj.sec}`)
+}
+// showTime(currentTime)
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+// создадим функцию которая будет приводить время к нормальному виду
+function timeAdjustment(obj) {
+    let hours = obj.hours
+    let min = obj.min
+    let sec = obj.sec
+    if(sec >= 60) {
+        min = min + Math.trunc(sec / 60)
+        sec = sec % 60
+    } 
+    if(min >= 60) {
+        hours = hours + Math.trunc(min / 60)
+        min = min % 60
+    } 
+    // console.log(hours, min, sec)  // все работает . будем мутировать входящий объект. т.к. таким образом скорректируем данные если они переданы уже первоначально не в том формате
+    obj.hours = hours
+    obj.min = min
+    obj.sec = sec
+}
+
+// console.log(currentTime)     // передаваемый объект мутировал . 
+
+// Функция изменения времени на переданное количество секунд.
+function addSeconds(obj, s) {
+    obj.sec = obj.sec + s
+    timeAdjustment(obj)
+}
+// addSeconds(currentTime, 80)
+
+
+// Функция изменения времени на переданное количество минут.
+function addMin(obj, m) {
+    obj.min = obj.min + m
+    timeAdjustment(obj)
+}
+// addMin(currentTime, 10)
+
+
+// Функция изменения времени на переданное количество  часов.
+function addHours(obj, h) {
+    obj.hours = obj.hours + h
+    timeAdjustment(obj)
+}
+// addHours(currentTime, 4)
+
+// showTime(currentTime)  // видим что все работает
+
+
